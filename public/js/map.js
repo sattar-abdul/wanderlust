bf575da254b92fc8e467b28c15131c8ff47c9673
em.email <heysunny.1212@gmail.com>
2025-05-26 00:05:43 +0530

    map marker is added for listing locations

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,13 @@
-const Key = maptilerKey; //key comming from show.ejs
+const Key = window.maptilerKey; //key comming from show.ejs
+const coords = window.coordinates;
+
 const map = new maplibregl.Map({
   container: "map",
   style: `https://api.maptiler.com/maps/streets/style.json?key=${Key}`,
-  center: [77.209, 28.6139], // [Longitude, Latitude]
+  center: coords, // [Longitude, Latitude]
   zoom: 10,
 });
+
+const marker = new maplibregl.Marker()
+  .setLngLat(coords)
+  .addTo(map);
