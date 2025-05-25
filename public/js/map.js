acb9a0b6adb3b5a2b667769f2501604f95aa5b21
em.email <heysunny.1212@gmail.com>
2025-05-26 01:18:57 +0530

    added custom map marker with popup

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,25 @@
-const Key = window.maptilerKey; //key comming from show.ejs
-const coords = window.coordinates;
+const Key = maptilerKey; //key comming from show.ejs
 
+//show New Map
 const map = new maplibregl.Map({
   container: "map",
   style: `https://api.maptiler.com/maps/streets/style.json?key=${Key}`,
-  center: coords, // [Longitude, Latitude]
+  center: listing.geometry.coordinates, // [Longitude, Latitude]
   zoom: 10,
 });
 
-const marker = new maplibregl.Marker()
-  .setLngLat(coords)
+//custom marker for map
+const el = document.createElement('div');
+el.className = 'custom-marker';
+el.innerHTML = '<i class="fa-solid fa-house"></i>';
+
+
+//Adding marker pointing to location
+const marker = new maplibregl.Marker({ element: el })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new maplibregl.Popup({ offset: 25 }).setHTML(
+      `<H5>${listing.location}</H5> <P>Exact location provided after booking.</P>`
+    )
+  )
   .addTo(map);
--- readme.md
+++ readme.md
@@ -34,5 +34,11 @@ used cloud service: cloudinary along with its packages
 .env to store credentials.
 
 ### For maps
-MapLibre's is used
-maptiler used for map tiles
+MapLibre is used
+maptiler used for map tiles (detailed varsion of map including lables, roads)
+
+Geocoding is the process of converting a human-readable address (new delhi) into geographic coordinates [28.6139, 77.2088], usually latitude and longitude.
+
+This process can also be done in reverse, where geographic coordinates are converted into a human-readable address, This is called reverse geocoding. 
+
+We have used `maptiler's api` for geocoding the locations and showing it on map.
