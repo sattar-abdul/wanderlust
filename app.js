fbdbaefe03534f96305b3169e6ef1bc81505ed21
em.email <heysunny.1212@gmail.com>
2025-05-27 23:03:22 +0530

    DB migrated to cloud: mongoDB Atlas

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ if (process.env.NODE_ENV != "production") {
 
 const express = require("express");
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"; //when using local db
+const dbUrl = process.env.ATLASDB_URL; //when using cloud db
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 
 const passport = require("passport");
@@ -41,7 +43,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 //express part
@@ -52,17 +54,30 @@ async function main() {
 // });
 
 //session- a session Id will be created for each user
-app.use(
-  session({
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
     secret: "mySuperSecretKey",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, //keeps session for 7days
-      httpOnly: true,
-    },
-  })
-);
+  },
+  touchAfter: 24 * 3600, //unmodified sessions are updated every 24hr
+});
+
+store.on("error", () => {
+  console.log("Error in Mongodb session store", err);
+});
+
+const sessionOptions = {
+  store: store,
+  secret: "mySuperSecretKey",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //keeps session for 7days
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
 app.use(flash());
 
 //authentication
